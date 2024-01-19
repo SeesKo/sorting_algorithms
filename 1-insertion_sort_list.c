d6e84c9bb0807fc05449da00e55fa4289ea0079c
Siskosee <seeskosee@seeskoseesko.com>
2024-01-18 16:05:29 -0800

    sorting using insert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,11 +4,11 @@
  * insertion_sort_list - Sorts a doubly linked list of
  * integers in ascending order using Insertion sort.
  *
- * @list: Pointer to the head of the list
+ * @list: Pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev, *next;
+	listint_t *prev, *current, *next;
 
 	if (list == NULL || *list == NULL)
 		return;
