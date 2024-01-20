5d3bb33e6ba3e0da8fa9cf8952a4873827c7b05b
Siskosee <seeskosee@seeskoseesko.com>
2024-01-20 13:41:36 -0800

    sorting using heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,60 +1,64 @@
 #include "sort.h"
 
 /**
- * heapify - Converts an array into a max heap.
- * @array: The array to be transformed
- * @size: Size of the array
- * @i: Index of the current root in the heap
- * @n: Size of the heap
+ * make_heap - Converts an array into a max heap.
+ * @array: The array to be converted.
+ * @size: Size of the array.
+ * @i: Index of the current root in the heap.
+ * @n: Size of the heap.
  */
-void heapify(int *array, size_t size, size_t i, size_t n)
+void make_heap(int *array, size_t size, size_t i, size_t n)
 {
-    size_t largest = i;
-    size_t left = 2 * i + 1;
-    size_t right = 2 * i + 2;
+	size_t largest, left, right;
+	int temp;
 
-    if (left < n && array[left] > array[largest])
-        largest = left;
+	largest = i;
+	left = 2 * i + 1;
+	right = 2 * i + 2;
 
-    if (right < n && array[right] > array[largest])
-        largest = right;
+	if (left < n && array[left] > array[largest])
+		largest = left;
 
-    if (largest != i)
-    {
-        int temp = array[i];
-        array[i] = array[largest];
-        array[largest] = temp;
+	if (right < n && array[right] > array[largest])
+		largest = right;
 
-        print_array(array, size);
-        heapify(array, size, largest, n);
-    }
+	if (largest != i)
+	{
+		temp = array[i];
+		array[i] = array[largest];
+		array[largest] = temp;
+
+		print_array(array, size);
+		make_heap(array, size, largest, n);
+	}
 }
 
 /**
- * heap_sort - Sorts an array of integers in ascending order using Heap sort
- *
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * heap_sort - Sorts an array of integers in ascending order
+ * using the Heap sort algorithm.
+ * @array: The array to be sorted.
+ * @size: Number of elements in the array.
  */
 void heap_sort(int *array, size_t size)
 {
 	size_t i;
+	int temp;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    /* Build the max heap */
-    for (i = size / 2 - 1; i != (size_t)-1; i--)
-        heapify(array, size, i, size);
+	/* Building the max heap */
+	for (i = size / 2 - 1; i != (size_t)-1; i--)
+		make_heap(array, size, i, size);
 
-    /* Extract elements from the heap one by one */
-    for (i = size - 1; i > 0; i--)
-    {
-        int temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
+	/* Extracting elements from the heap one by one */
+	for (i = size - 1; i > 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
 
-        print_array(array, size);
-        heapify(array, size, 0, i);
-    }
+		print_array(array, size);
+		make_heap(array, size, 0, i);
+	}
 }
