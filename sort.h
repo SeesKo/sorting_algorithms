eab1c40f71f9a8e286cea1b7f94bf0f0f0b32433
Siskosee <seeskosee@seeskoseesko.com>
2024-01-20 07:10:20 -0800

    main header

--- sort.h
+++ sort.h
@@ -30,5 +30,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+void swap_nodes(listint_t *a, listint_t *b, listint_t **list);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
