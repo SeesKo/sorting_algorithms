4c65427c576110c126496d778a5be3673a4b532d
Siskosee <seeskosee@seeskoseesko.com>
2024-01-22 15:24:12 -0800

    main header

--- sort.h
+++ sort.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /* STRUCTS */
 /**
  * struct listint_s - Doubly linked list node.
@@ -32,34 +33,25 @@ void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
-
 void cocktail_sort_list(listint_t **list);
 void swapNode(listint_t **list, listint_t *node1, listint_t *node2);
-
 void counting_sort(int *array, size_t size);
-
 void merge(int *array, size_t l, size_t m, size_t r);
 void merge_sort_recursive(int *array, size_t l, size_t r);
 void merge_sort(int *array, size_t size);
 void merge_subarrays(int *array, size_t l, size_t m, size_t r,
 		int *temp_array);
-
 void make_heap(int *array, size_t size, size_t i, size_t n);
 void heap_sort(int *array, size_t size);
-
 int getting_max(int *array, size_t size);
 void countSort(int *array, size_t size, int exponent);
 void radix_sort(int *array, size_t size);
-
-
 void swap(int *a, int *b);
 int hoare_partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 void bitonic_sort(int *array, size_t size);
 void bitonic_recursive(int *array, size_t size, int dir);
 void bitonic_merge(int *array, size_t size, int dir);
-
 
 #endif /* SORT_H */
