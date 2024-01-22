dbcde59447b1794ccea925157eb1e1e2219042df
Siskosee <seeskosee@seeskoseesko.com>
2024-01-22 07:33:47 -0800

    main header

--- sort.h
+++ sort.h
@@ -57,4 +57,9 @@ int hoare_partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
+void bitonic_sort(int *array, size_t size);
+void bitonic_recursive(int *array, size_t size, int dir);
+void bitonic_merge(int *array, size_t size, int dir);
+
+
 #endif /* SORT_H */
